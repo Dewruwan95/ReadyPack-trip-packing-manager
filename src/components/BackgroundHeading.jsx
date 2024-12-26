64820ff912b8fe30c4f5bd632249e822239a8b7b
Ositha Dewruwan <dewruwanebay@gmail.com>
2024-12-26 22:24:14 +0530

    created secontary components structure

--- src/components/BackgroundHeading.jsx
+++ src/components/BackgroundHeading.jsx
@@ -1,3 +1,3 @@
 export default function BackgroundHeading() {
-  return <h1>Ready Pack</h1>;
+  return <h1>ReadyPack</h1>;
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,11 @@
+import Counter from "./Counter";
+import Logo from "./Logo";
+
 export default function Header() {
-  return <div>HEADER</div>;
+  return (
+    <header>
+      <Logo />
+      <Counter />
+    </header>
+  );
 }
