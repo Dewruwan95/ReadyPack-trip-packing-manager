e273fe8d575f3713f979dffa40a1772c382b8968
Ositha Dewruwan <dewruwanebay@gmail.com>
2024-12-27 21:18:25 +0530

    created add new item functionality

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,11 +1,5 @@
+import { secondaryButtons } from "../lib/constants";
 import Button from "./Button";
-
-const secondaryButtons = [
-  "Mark all as completed",
-  "Mark all as incompleted",
-  "Reset to initial",
-  "Remove all items",
-];
 
 export default function ButtonGroup() {
   return (
---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const initialItems = [
+  { id: 1, name: "good mode", packed: true },
+  { id: 2, name: "passport", packed: false },
+  { id: 3, name: "phone charger", packed: false },
+];
+
+export const secondaryButtons = [
+  "Mark all as completed",
+  "Mark all as incompleted",
+  "Reset to initial",
+  "Remove all items",
+];
